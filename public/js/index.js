36ec920711a458aa591d4f1ae20ae09804caea2f
Ankit Chaurasia <ac13793@gmail.com>
2018-11-03 21:40:07 +0530

    Broadcasting Events

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', () => {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'user@create',
-    text: 'createMessage'
-  });
 });
 
 socket.on('disconnect', () => console.log('Disconnected from server'));
--- server/server.js
+++ server/server.js
@@ -17,13 +17,33 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.emit('newMessage', {
-    from: 'user@newMessage',
-    text: 'newMessage',
-    createdAt: '123'
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
   });
 
   socket.on('createMessage', (newMessage) => {
     console.log('New message', newMessage);
+    io.emit('newMessage', {
+      from: newMessage.from,
+      text: newMessage.text
+    });
+    socket.broadcast.emit('newMessage', {
+      from: newMessage.from,
+      text: newMessage.text,
+      createdAt: new Date().getTime()
+    });
+    socket.emit('newMessage', {
+      from: newMessage.from,
+      text: newMessage.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => console.log('User was disconnected'));
