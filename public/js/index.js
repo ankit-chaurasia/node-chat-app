c39080e84bccff6faba5fc834f182b9c1df90a72
Ankit Chaurasia <ac13793@gmail.com>
2018-11-04 13:26:57 +0530

    Moustache.js

--- public/js/index.js
+++ public/js/index.js
@@ -9,20 +9,25 @@ socket.on('disconnect', () => console.log('Disconnected from server'));
 socket.on('newMessage', (newMessage) => {
   const { from, text, createdAt } = newMessage;
   const formattedTime = moment(createdAt).format('h:mm a');
-  let li = $('<li></li>');
-  li.text(`${from} ${formattedTime}: ${text}`);
-  $('#messages').append(li);
+  const template = $('#message-template').html();
+  const html = Mustache.render(template, {
+    from,
+    text,
+    createdAt: formattedTime
+  });
+  $('#messages').append(html);
 });
 
 socket.on('newLocationMessage', (newLocationMessage) => {
   const { from, url, createdAt } = newLocationMessage;
   const formattedTime = moment(createdAt).format('h:mm a');
-  let li = $('<li></li>');
-  let a = $('<a target="_blank">My Current Location</a>');
-  li.text(`${from} ${formattedTime}: `);
-  a.attr('href', url);
-  li.append(a);
-  $('#messages').append(li);
+  const template = $('#location-message-template').html();
+  const html = Mustache.render(template, {
+    from,
+    url,
+    createdAt: formattedTime
+  });
+  $('#messages').append(html);
 });
 
 $('#message-form').on('submit', (e) => {
